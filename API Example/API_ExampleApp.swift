d1213eb80de3a3afcdf2bfc2687e9e5350b90a0b
Lorenzo Murillo <Lorenzo.murillo.iv@gmail.com>
2025-05-12 19:28:02 -0700

    Initial Commit

---
+++ API Example/API_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ExampleApp.swift
+//  API Example
+//
+//  Created by Lorenzo Murillo IV on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct API_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API ExampleTests/API_ExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  API_ExampleTests.swift
+//  API ExampleTests
+//
+//  Created by Lorenzo Murillo IV on 5/12/25.
+//
+
+import Testing
+
+struct API_ExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
